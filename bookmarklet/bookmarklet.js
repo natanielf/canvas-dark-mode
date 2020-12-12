1919816953b79fe0c0c522a3af3ff96aac9063e0
natanielf <49921489+natanielf@users.noreply.github.com>
2020-12-12 13:46:09 -0800

    Create bookmarklet.js

---
+++ bookmarklet/bookmarklet.js
@@ -0,0 +1,14 @@
+const $$ = (selector) => document.querySelectorAll(selector)
+const createElement = (tagName, properties) => Object.assign(document.createElement(tagName), properties)
+
+// Remove all CSS stylesheets, external and internal
+$$('link[rel="stylesheet"],style').forEach((el) => el.remove())
+
+// Remove all inline styles
+$$('*').forEach((el) => (el.style = ''))
+
+document.head.append(
+  createElement('link', {
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/gh/natanielf/canvas-dark-mode/bookmarklet/canvas-dark-mode.css'
+  }),
